11dda6e3f853749339e7f375a5c33200cd4fa44c
Chase-V <38985553+Chase-V@users.noreply.github.com>
2021-09-06 23:03:46 +0300

    Initial commit

---
+++ app/src/main/java/com/tashev/kotlinhw1/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.tashev.kotlinhw1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
